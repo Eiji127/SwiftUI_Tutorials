0d066106b749b8ce0e4978560a5809650eb58b3d
Eiji127 <64912886+Eiji127@users.noreply.github.com>
2022-04-04 23:48:42 +0900

    [Upload]: add new Project 'Landmarks'

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 白数叡司 on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
