4c9dc96217e0cc6a09815819cc3dfc75cf3ba129
Eiji127 <64912886+Eiji127@users.noreply.github.com>
2022-04-13 15:07:02 +0900

    [Update]: Create the List of Landmarks

---
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 白数叡司 on 2022/04/13.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
