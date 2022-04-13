f4c7f373d9eefbc9fc6ca5718c343d0a29fb407e
Eiji127 <64912886+Eiji127@users.noreply.github.com>
2022-04-13 13:44:03 +0900

    [Update]: Create the Row View

---
+++ Landmarks/Landmarks/LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by 白数叡司 on 2022/04/13.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
