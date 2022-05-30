adc56bfedc6202a6f9b69bff835d2a6fecbb3fe6
Eiji127 <64912886+Eiji127@users.noreply.github.com>
2022-05-30 21:28:38 +0900

    [Update]: Draw the Badge Background

---
+++ DrawingAndAnimation/DrawingAndAnimation/DrawingAndAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAndAnimationApp.swift
+//  DrawingAndAnimation
+//
+//  Created by 白数叡司 on 2022/05/28.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAndAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
